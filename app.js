85fbce7bde9eb1edebacc649af8fd49df471ff7d
pythexcel <pythexcel@excellencetechnologies.in>
2021-11-12 14:24:58 +0100

    removed cors error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let express = require("express");
 let path = require("path");
 let cookieParser = require("cookie-parser");
 // let logger = require("morgan");
-// const cors = require('cors')
+const cors = require('cors')
 let { root, users, homePage, college, jobs, course ,states, news} = require("./routes");
 
 let app = express();
@@ -12,7 +12,7 @@ let app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-// app.use(cors());
+app.use(cors());
 // app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
